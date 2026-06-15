fc76a0d5879daf909e8600caaef5b834d2268d11
VA <nastulua@yandex.ru>
2026-06-15 16:48:04 +0300

    онтарио

--- script/products_lists/allProducts.js
+++ script/products_lists/allProducts.js
@@ -1015,6 +1015,11 @@ const knownProductsAllFiles = [
     "is_archive": false
   },
   {
+    "name": "онтарио в пластик хром",
+    "category": "armchair_rukovoditel",
+    "is_archive": false
+  },
+  {
     "name": "Оптима м-901 с подголовником черный пластик",
     "category": "armchair_comfort",
     "is_archive": false
@@ -1707,11 +1712,6 @@ const knownProductsAllFiles = [
   {
     "name": "Cильвия хром",
     "category": "chair_visitors",
-    "is_archive": false
-  },
-  {
-    "name": "ch-105 онтарио в пластик хром",
-    "category": "armchair_rukovoditel",
     "is_archive": false
   },
   {
